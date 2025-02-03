210679a3f68662bdd911374c2469f652897f9ebe
Fanekena-dev <fanekenaraharijaona@gmail.com>
2025-02-03 18:52:05 +0100

    Update landingPage.php
    Need to add <form>

--- app/views/landingPage.php
+++ app/views/landingPage.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Breeding</title>
 </head>
+
 <body>
   Landing Page for our website
+  <p>
+    Tokony formulaire
+    <a href="">Sign In</a>
+  </p>
+  <p>
+    Tokony formulaire
+    <a href="">Sign Up</a>
+  </p>
+  <footer>
+    <p>3251 - 3286 - 3291</p>
+    <a href="/admin/login">Admin</a>
+  </footer>
 </body>
+
 </html>
