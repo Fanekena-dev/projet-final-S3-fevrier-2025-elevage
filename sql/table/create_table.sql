d62c909440f1deb449511a482528af3e9c755d43
gigasandwich <yvannandy@gmail.com>
2025-02-03 20:23:59 +0300

    Fix table_schema.txt and create_table.sql

--- sql/table/create_table.sql
+++ sql/table/create_table.sql
@@ -1,17 +1,22 @@
-CREATE DATABASE IF NOT EXISTS breeding;
+CREATE DATABASE breeding;
 USE breeding;
 
--- Tables for species
 CREATE TABLE breeding_animal_species (
     species_id VARCHAR(70) PRIMARY KEY,
     species_name TEXT
+);
+
+CREATE TABLE breeding_animal (
+    animal_id VARCHAR(70) PRIMARY KEY,
+    animal_name TEXT,
+    description TEXT
 );
 
 CREATE TABLE breeding_species_min_weight_sale (
     min_sale_weight_id VARCHAR(70) PRIMARY KEY,
     species_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
-    min_sales_weight DECIMAL(7, 2),
+    min_sales_weight DECIMAL(7,2),
     FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id)
 );
 
@@ -19,7 +24,7 @@ CREATE TABLE breeding_species_max_weight (
     max_weight_id VARCHAR(70) PRIMARY KEY,
     species_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
-    max_weight DECIMAL(7, 2),
+    max_weight DECIMAL(7,2),
     FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id)
 );
 
@@ -27,7 +32,7 @@ CREATE TABLE breeding_species_selling_price (
     selling_price_id VARCHAR(70) PRIMARY KEY,
     species_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
-    selling_price DECIMAL(15, 2),
+    selling_price DECIMAL(15,2),
     FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id)
 );
 
@@ -47,12 +52,11 @@ CREATE TABLE breeding_species_weight_loss (
     FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id)
 );
 
--- Tables for users
 CREATE TABLE breeding_user (
     user_id VARCHAR(70) PRIMARY KEY,
     username TEXT,
     real_name TEXT,
-    insert_date DATE,
+    insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     pwd TEXT,
     email TEXT,
     numero TEXT
@@ -61,21 +65,20 @@ CREATE TABLE breeding_user (
 CREATE TABLE breeding_user_wallet (
     wallet_id VARCHAR(70) PRIMARY KEY,
     user_id VARCHAR(70),
-    money DECIMAL(15, 2),
+    money DECIMAL(15,2),
     description TEXT,
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES breeding_user(user_id)
 );
 
--- Tables for animal
 CREATE TABLE breeding_user_animal_mvt (
     mvt_id VARCHAR(70) PRIMARY KEY,
     animal_id VARCHAR(70),
     user_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     mvt_type VARCHAR(3),
-    mvt_price DECIMAL(15, 2),
-    FOREIGN KEY (animal_id) REFERENCES breeding_animal_weight(animal_id),
+    mvt_price DECIMAL(15,2),
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id),
     FOREIGN KEY (user_id) REFERENCES breeding_user(user_id)
 );
 
@@ -84,7 +87,7 @@ CREATE TABLE breeding_animal_day_without_eating (
     animal_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     day_without_eating INT,
-    FOREIGN KEY (animal_id) REFERENCES breeding_animal_weight(animal_id)
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id)
 );
 
 CREATE TABLE breeding_animal_weight_loss (
@@ -92,20 +95,21 @@ CREATE TABLE breeding_animal_weight_loss (
     animal_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     weight_loss_percent INT,
-    FOREIGN KEY (animal_id) REFERENCES breeding_animal_weight(animal_id)
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id)
 );
 
 CREATE TABLE breeding_animal_weight (
     animal_id VARCHAR(70) PRIMARY KEY,
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
-    weight DECIMAL(7, 2)
+    weight DECIMAL(7,2),
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id)
 );
 
 CREATE TABLE breeding_animal_pic (
     pic_id VARCHAR(70) PRIMARY KEY,
     animal_id VARCHAR(70),
     filename TEXT,
-    FOREIGN KEY (animal_id) REFERENCES breeding_animal_weight(animal_id)
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id)
 );
 
 CREATE TABLE breeding_animal_market_mvt (
@@ -114,12 +118,11 @@ CREATE TABLE breeding_animal_market_mvt (
     user_id VARCHAR(70),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     mvt_type INT,
-    mvt_price DECIMAL(15, 2),
-    FOREIGN KEY (animal_id) REFERENCES breeding_animal_weight(animal_id),
+    mvt_price DECIMAL(15,2),
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id),
     FOREIGN KEY (user_id) REFERENCES breeding_user(user_id)
 );
 
--- Tables for food
 CREATE TABLE breeding_food (
     food_id VARCHAR(70) PRIMARY KEY,
     food_name TEXT
@@ -128,15 +131,59 @@ CREATE TABLE breeding_food (
 CREATE TABLE breeding_food_price (
     food_price_id VARCHAR(70) PRIMARY KEY,
     food_id VARCHAR(70),
-    price DECIMAL(15, 2),
+    price DECIMAL(15,2),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
+);
+
+CREATE TABLE breeding_species_food_weight_ratio (
+    sfwr_id VARCHAR(70) PRIMARY KEY,
+    food_id VARCHAR(70),
+    species_id VARCHAR(70),
+    ratio DECIMAL(7,2),
+    FOREIGN KEY (food_id) REFERENCES breeding_food(food_id),
+    FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id)
+);
+
+CREATE TABLE breeding_animal_food_weight_ratio (
+    sfwr_id VARCHAR(70) PRIMARY KEY,
+    food_id VARCHAR(70),
+    animal_id VARCHAR(70),
+    ratio DECIMAL(7,2),
+    FOREIGN KEY (food_id) REFERENCES breeding_food(food_id),
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id)
+);
+
+CREATE TABLE breeding_species_food (
+    species_food_id VARCHAR(70) PRIMARY KEY,
+    species_id VARCHAR(70),
+    food_id VARCHAR(70),
+    FOREIGN KEY (species_id) REFERENCES breeding_animal_species(species_id),
+    FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
+);
+
+CREATE TABLE breeding_animal_food (
+    animal_food_id VARCHAR(70) PRIMARY KEY,
+    animal_id VARCHAR(70),
+    food_id VARCHAR(70),
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id),
+    FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
+);
+
+CREATE TABLE breeding_animal_alimentation (
+    alimentation_id VARCHAR(70) PRIMARY KEY,
+    animal_id VARCHAR(70),
+    food_id VARCHAR(70),
+    food_weight DECIMAL(7,2),
+    insert_date DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (animal_id) REFERENCES breeding_animal(animal_id),
     FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
 );
 
 CREATE TABLE breeding_food_stock (
     food_stock_id VARCHAR(70) PRIMARY KEY,
     food_id VARCHAR(70),
-    food_weight DECIMAL(7, 2),
+    food_weight DECIMAL(7,2),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
 );
@@ -144,7 +191,7 @@ CREATE TABLE breeding_food_stock (
 CREATE TABLE breeding_food_market_stock (
     food_market_stock_id VARCHAR(70) PRIMARY KEY,
     food_id VARCHAR(70),
-    food_weight DECIMAL(7, 2),
+    food_weight DECIMAL(7,2),
     insert_date DATE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (food_id) REFERENCES breeding_food(food_id)
 );
