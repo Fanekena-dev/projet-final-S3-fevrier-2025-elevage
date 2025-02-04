9d67b73f858c6c1d11de91bd04532e79013eaf82
Fanekena-dev <fanekenaraharijaona@gmail.com>
2025-02-04 06:33:56 +0100

    Update routes.php
    species update

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,9 +1,9 @@
 <?php
-
-use app\controllers\w1\AdminController;
 use flight\Engine;
 use flight\net\Router;
 
+use app\controllers\w1\AdminController;
+use app\controllers\w1\SpeciesController;
 use app\controllers\w1\LandingPageController;
 use app\controllers\w1\AdminSigninController;
 use app\middlewares\w1\PrePostFormMiddleware;
@@ -21,13 +21,25 @@ $router->get('/', [LandingPageController::class, 'landingPage']);
 // # w1 routes
 // ===========
 $router->group(
+  '/admin/sign-in',
+  function () use ($router) {
+    $router->get('/', [AdminSigninController::class, 'signinPage']);
+    $router->post('/check', function () {
+      (new AdminSigninController())->signin($_POST['admin-id'], $_POST['admin-pwd']);
+    })
+      ->addMiddleware([new PrePostFormMiddleware((new AdminSigninController())->get_required_fields())]);
+  }
+);
+
+$router->group(
   '/admin',
   function () use ($router) {
-    $router->get('/sign-in', [AdminSigninController::class, 'signinPage']);
-    $router->post('/sign-in/check', function () {
-      (new AdminSigninController())->signin($_POST['admin-id'], $_POST['admin-pwd']); })
-      ->addMiddleware([new PrePostFormMiddleware((new AdminSigninController())->get_required_fields())]);
-    $router->get('/home', [AdminController::class, 'homePage']);
+    $router->get('/species', [AdminController::class, 'species']);
+    $router->get('/species/updatable-info', [SpeciesController::class, 'getSpeciesUpdatableInfo']);
+    $router->post('/species/update', function () {
+      (new SpeciesController())->bigUpdate($_POST['species']);
+    })
+      ->addMiddleware([new PrePostFormMiddleware(['species'])]);
   }
 );
 
